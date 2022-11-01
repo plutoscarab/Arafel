0484296d4b2dcaa16fda38cce2f301fb05cb6261
plutoscarab <bret_x@hotmail.com>
2022-11-01 15:58:33 -0700

    F# ebnf parser, start of Arafel lexer

---
+++ Ebnf.fs
@@ -0,0 +1,73 @@
+module Ebnf
+
+let scan line = 
+    seq {
+        let mutable depth = 0
+        let mutable lit = false
+        let mutable index = 0
+
+        for ch in line do
+            match ch with
+            | '\'' -> lit <- not lit
+            | '(' -> depth <- depth + if lit then 0 else 1
+            | ')' -> depth <- depth - if lit then 0 else 1
+            | _ -> lit <- lit
+            yield (index, ch, depth)
+            index <- index + 1
+    }
+
+type Multiplicity = ZeroOrOne | ZeroOrMore | OneOrMore
+
+let showMultiplicity m =
+    match m with
+    | ZeroOrOne -> "?"
+    | ZeroOrMore -> "*"
+    | OneOrMore -> "+"
+
+type EbnfExpr =
+    | Choice of EbnfExpr list
+    | Sequence of EbnfExpr list
+    | Primary of Multiplicity * EbnfExpr
+    | Parens of EbnfExpr
+    | StringLiteral of string
+    | NcName of string
+
+let rec show expr =
+    match expr with
+    | Choice list -> list |> List.map show |> String.concat " | "
+    | Sequence list -> list |> List.map show |> String.concat " "
+    | Primary (m, e) -> (show e) + (showMultiplicity m)
+    | Parens e -> "(" + (show e) + ")"
+    | StringLiteral s -> "'" + s + "'"
+    | NcName n -> n
+
+let rec parseItem (s:string) =
+    match s[s.Length - 1] with
+    | '?' -> Primary (ZeroOrOne, parseItem (s.Substring(0, s.Length - 1)))
+    | '*' -> Primary (ZeroOrMore, parseItem (s.Substring(0, s.Length - 1)))
+    | '+' -> Primary (OneOrMore, parseItem (s.Substring(0, s.Length - 1)))
+    | ')' -> Parens (parseExpr (s.Substring(1, s.Length - 2)))
+    | '\'' -> StringLiteral (s.Substring(1, s.Length - 2))
+    | _ -> NcName s
+
+and parseSequence (s:string) =
+    let list = scan s |> Seq.filter (fun (index, ch, depth) -> ch = ' ' && depth = 0)
+    let mutable i = 0
+    let mutable items = []
+    for (index, _, pipe) in list do
+        items <- if index > i then s.Substring(i, index - i) :: items else items
+        i <- index + 1
+    match items with
+    | [] -> parseItem (s.Substring(i))
+    | _ -> Sequence (s.Substring(i) :: items |> List.rev |> List.map parseItem)
+
+and parseExpr (s:string) =
+    let list = scan s |> Seq.filter (fun (index, ch, depth) -> ch = '|' && depth = 0)
+    let mutable i = 0
+    let mutable choices = []
+    for (index, _, pipe) in list do
+        choices <- if index > i + 1 then s.Substring(i, index - i - 1).Trim() :: choices else choices
+        i <- index + 1
+    match choices with
+    | [] -> parseSequence (s.Substring(i).Trim())
+    | _ -> Choice (s.Substring(i).Trim() :: choices |> List.rev |> List.map parseSequence)
--- Program.fs
+++ Program.fs
@@ -1,3 +1,118 @@
+open System
+open System.Globalization
 open System.IO
+open System.Linq
+open System.Numerics
+open System.Text
 
-let grammarSrc = File.ReadAllText "core.grammar.txt"
+type Cursor =
+    { source: Rune array;
+      index: int;
+      line: int;
+      pos: int }
+
+    member c.More = c.index < c.source.Length
+
+    member c.Current = if c.More then c.source[c.index] else new Rune(0)
+
+    member c.Str = c.Current.ToString()
+
+    member c.Next = 
+        if c.More then 
+            match c.Str with
+            | "\n" | "\r" when c.index > c.source.Length - 2 || c.source[c.index + 1].ToString() <> "\n" -> 
+                { c with index = c.index + 1; line = c.line + 1; pos = 1 }
+            | _ ->
+                { c with index = c.index + 1; pos = c.pos + 1 }
+        else c
+
+let makeCursor (source:string) =
+    let runes = source.EnumerateRunes() |> Array.ofSeq
+    { source = runes; index = 0; line = 1; pos = 1 }
+
+type Span = Cursor * Cursor
+
+let spanStr (s:Span) =
+    let source = (fst s).source
+    [(fst s).index .. ((snd s).index - 1)] |> Seq.map (fun index -> source[index]) |> Seq.map (fun rune -> rune.ToString()) |> String.concat ""
+
+type Token = 
+    | Identifier of Span
+    | Operator of Span
+    | Nat of Span
+    | String of Span
+    | Error of Cursor
+
+let tokenStr (token:Token) =
+    match token with
+    | Identifier span -> "Identifier " + (spanStr span)
+    | Operator span -> "Operator " + (spanStr span)
+    | Nat span -> "Nat " + (spanStr span)
+    | String span -> "String " + (spanStr span)
+    | Error c -> "Error " + (c.Str)
+
+let isWhitespace r =
+    let uc = Rune.GetUnicodeCategory r
+    uc = UnicodeCategory.SpaceSeparator || uc = UnicodeCategory.LineSeparator
+
+let tokenise (cursor:Cursor) =
+    seq {
+        let mutable c = cursor
+        let mutable line = 0
+
+        while c.More do
+            if line <> c.line && not (isWhitespace (c.Current)) then
+                while line = c.line do
+                    c <- c.Next
+                line <- c.line
+            else
+                let uc = Rune.GetUnicodeCategory (c.Current)
+                match c.Current with
+                | r when isWhitespace r -> 
+                    while isWhitespace (c.Current) do
+                        c <- c.Next
+                | r when Rune.IsLetter r || c.Str = "_" ->
+                    let mutable start = c
+                    while Rune.IsLetter (c.Current) || Rune.IsDigit (c.Current) || c.Str = "_" do
+                        c <- c.Next
+                    yield Identifier (start, c)
+                | r when Rune.IsDigit r ->
+                    let mutable start = c
+                    while Rune.IsDigit (c.Current) do
+                        c <- c.Next
+                    yield Nat (start, c)
+                | r when uc = UnicodeCategory.MathSymbol || uc = UnicodeCategory.OtherSymbol ->
+                    let mutable start = c
+                    while Rune.GetUnicodeCategory (c.Current) = UnicodeCategory.MathSymbol || Rune.GetUnicodeCategory (c.Current) = UnicodeCategory.OtherSymbol do
+                        c <- c.Next
+                    yield Operator (start, c)
+                | _ ->
+                    yield Error c
+                    c <- c.Next
+    } |> Array.ofSeq
+
+let main =
+    for line in File.ReadAllLines "core.grammar.txt" do
+        let pair = line.Split("::=")
+        printf "%s ::= " (pair[0].Trim())
+        let expr = Ebnf.parseExpr (pair[1].Trim())
+        printfn "%s" (Ebnf.show expr)
+
+    let src = File.ReadAllText("core.af")
+    let cursor = makeCursor src
+    Console.OutputEncoding <- Encoding.Unicode
+    let mutable c = cursor
+
+    while c.More do
+        if c.pos = 1 then
+            printf "Line %d: " (c.line)
+        printf "%s" (c.Str)
+        c <- c.Next
+
+    printfn ""
+
+    for token in tokenise cursor do
+        printf "%s, " (tokenStr token)
+    
+    0
+
